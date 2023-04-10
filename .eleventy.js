f9d423074cfe20affb4e57dcb0da6e8ce0c04bff
dustin-jw <dustin@heysparkbox.com>
2023-04-10 15:27:43 -0600

    chore: configure serverless plugin and Netlify redirects

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const { EleventyServerlessBundlerPlugin } = require('@11ty/eleventy');
 const { marked } = require('marked');
 const DOMPurify = require('isomorphic-dompurify');
 
@@ -16,6 +17,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addLayoutAlias('default', 'layout.njk');
 
   eleventyConfig.addFilter('mdToHtml', markdownToHtml);
+
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: 'serverless',
+    functionsDir: './netlify/functions/',
+  });
 
   return {
     dir: {
