5a1df6f38fd75285a7729dc0a44f0c16ded1eda4
dustin-jw <dustin@heysparkbox.com>
2023-04-10 12:14:31 -0600

    feat: fetch question data from airtable

---
+++ pages/_data/questions.js
@@ -0,0 +1,27 @@
+/* eslint-env node */
+require('dotenv').config();
+const Airtable = require('airtable');
+
+Airtable.configure({
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: process.env.AIRTABLE_API_KEY,
+});
+
+const base = Airtable.base(process.env.AIRTABLE_BASE_ID);
+
+const getPublishedQuestions = async () => {
+  const questions = await base('Questions and Answers').select().all();
+
+  const publishedQuestions = questions
+    .filter((record) => record.fields.Published)
+    .map((record) => record.fields);
+  console.log(JSON.stringify(publishedQuestions, null, 2));
+
+  return publishedQuestions;
+};
+
+module.exports = async () => {
+  const questions = await getPublishedQuestions();
+
+  return questions;
+};
