f638a43c8d9a169922e4bad1745522f3f2ffbbf8
dustin-jw <dustin@heysparkbox.com>
2023-04-25 10:14:12 -0600

    feat: add macro and script for short answer form

---
+++ src/js/short-answer.js
@@ -0,0 +1,13 @@
+const answerForm = document.querySelector('#answerForm');
+const acceptedAnswer = document.querySelector('#acceptedAnswer');
+const acceptedAnswerSection = document.querySelector('#acceptedAnswerSection');
+
+const handleShortAnswerFormSubmit = (event) => {
+  event.preventDefault();
+
+  const acceptedAnswerContent = acceptedAnswer.innerHTML;
+  acceptedAnswerSection.innerHTML = acceptedAnswerContent;
+  acceptedAnswerSection.focus();
+};
+
+answerForm.addEventListener('submit', handleShortAnswerFormSubmit);
