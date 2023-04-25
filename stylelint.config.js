59bde2672f717ef8ce24af40a4f6394e2c7f828b
zastrow <philip@zastrow.co>
2023-04-25 15:58:47 -0400

    feat: update linting to use standard-scss

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  extends: ['stylelint-config-standard'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recommended-scss'
+  ],
   rules: {
     'selector-class-pattern': null,
   },
