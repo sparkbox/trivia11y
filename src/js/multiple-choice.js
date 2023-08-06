fb761b5b668d192c4ad59a3c15322593694950e2
Theo Gainey <theogainey@gmail.com>
2023-08-06 15:07:10 -0400

    feat: Randomize Multiple Choice Answers
    - add runtime JS function to randomize multiple choice answer order

--- src/js/multiple-choice.js
+++ src/js/multiple-choice.js
@@ -21,3 +21,13 @@ const handleOptionButtonClick = (event) => {
 optionButtons.forEach((button) => {
   button.addEventListener('click', handleOptionButtonClick);
 });
+
+const randomizeAnswers = () => {
+  const multipleChoiceItems = document.querySelectorAll('.cmp-multiple-choice__item[data-needs-js]');
+  const randomizedOrder = Array.from(multipleChoiceItems).sort(() => Math.random() > 0.5 ? 1 : -1);
+  const multipleChoiceItemsContainer = document.querySelector('.cmp-multiple-choice');
+  randomizedOrder.forEach((element) => {
+    multipleChoiceItemsContainer.append(element);
+  });
+};
+randomizeAnswers();
