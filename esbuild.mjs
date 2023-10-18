cebbaaaa37a7eabe6b0e425ac2ddf59fda5f9ded
dustin-jw <dustin@heysparkbox.com>
2023-10-18 10:58:45 -0600

    chore: incorporate esbuild into build process

---
+++ esbuild.mjs
@@ -0,0 +1,26 @@
+import * as esbuild from 'esbuild';
+import { config } from 'dotenv';
+
+config();
+
+const isProduction = process.env.NODE_ENV !== 'development';
+
+const options = {
+  entryPoints: ['src/js/*.js'],
+  format: 'esm',
+  bundle: true,
+  splitting: true,
+  outdir: 'dist/js',
+  // run `npx browserslist` to find updated minimum versions to set here
+  target: ['chrome109', 'edge109', 'firefox109', 'safari15.5', 'opera94'],
+  sourcemap: isProduction ? false : 'inline',
+  minify: isProduction,
+  logLevel: 'info',
+};
+
+if (!isProduction) {
+  let context = await esbuild.context(options);
+  await context.watch();
+} else {
+  await esbuild.build(options);
+}
