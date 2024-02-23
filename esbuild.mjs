9e17e36fd5c996898c79d3be5f0b26b19e7e256d
dustin-jw <dustin@heysparkbox.com>
2024-02-23 14:44:48 -0700

    style: add clarifying comments

--- esbuild.mjs
+++ esbuild.mjs
@@ -6,21 +6,23 @@ config();
 const isProduction = process.env.NODE_ENV !== 'development';
 
 const options = {
-  entryPoints: ['src/js/*.js'],
-  format: 'esm',
-  bundle: true,
-  splitting: true,
-  outdir: 'dist/js',
-  // run `npx browserslist` to find updated minimum versions to set here
-  target: ['chrome109', 'edge109', 'firefox109', 'safari15.5', 'opera94'],
-  sourcemap: isProduction ? false : 'inline',
-  minify: isProduction,
-  logLevel: 'info',
+	entryPoints: ['src/js/*.js'],
+	format: 'esm',
+	bundle: true,
+	splitting: true,
+	outdir: 'dist/js',
+	// run `npx browserslist` to find updated minimum versions to set here
+	target: ['chrome109', 'edge109', 'firefox109', 'safari15.5', 'opera94'],
+	sourcemap: isProduction ? false : 'inline',
+	minify: isProduction,
+	logLevel: 'info',
 };
 
 if (!isProduction) {
-  let context = await esbuild.context(options);
-  await context.watch();
+	// watch for changes and rebuild during development
+	let context = await esbuild.context(options);
+	await context.watch();
 } else {
-  await esbuild.build(options);
+	// build once and exit
+	await esbuild.build(options);
 }
