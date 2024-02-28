a4f2777173e63b79d5011e7ee96035adf204c032
dustin-jw <dustin@heysparkbox.com>
2024-02-28 10:43:21 -0700

    feat: update question total number in heading/title

--- src/js/helpers/update-pagination-links.js
+++ src/js/helpers/update-pagination-links.js
@@ -1,97 +1,91 @@
 const removeLink = (selector) => {
-  const span = document.createElement('span');
-  const link = document.querySelector(selector);
-  link?.replaceWith(span);
+	const span = document.createElement('span');
+	const link = document.querySelector(selector);
+	link?.replaceWith(span);
 };
 
 const setLinkHref = (selector, url, linkText) => {
-  const link = document.querySelector(selector);
+	const link = document.querySelector(selector);
 
-  if (link) {
-    link.setAttribute('href', url);
-  } else {
-    const span = document.querySelector('.cmp-pagination span');
-    const a = document.createElement('a');
-    a.setAttribute('href', url);
-    a.setAttribute(selector.replace('[', '').replace(']', ''), '');
-    a.innerHTML = linkText;
-    span?.replaceWith(a);
-  }
+	if (link) {
+		link.setAttribute('href', url);
+	} else {
+		const span = document.querySelector('.cmp-pagination span');
+		const a = document.createElement('a');
+		a.setAttribute('href', url);
+		a.setAttribute(selector.replace('[', '').replace(']', ''), '');
+		a.innerHTML = linkText;
+		span?.replaceWith(a);
+	}
 };
 
 const updatePreviousLink = (questions, currentQuestionIndex) => {
-  if (currentQuestionIndex <= 0) {
-    removeLink('[data-prev-link]');
-  } else {
-    setLinkHref(
-      '[data-prev-link]',
-      questions[currentQuestionIndex - 1],
-      'Previous Question'
-    );
-  }
+	if (currentQuestionIndex <= 0) {
+		removeLink('[data-prev-link]');
+	} else {
+		setLinkHref('[data-prev-link]', questions[currentQuestionIndex - 1], 'Previous Question');
+	}
 };
 
 const updateNextLink = (questions, currentQuestionIndex) => {
-  if (currentQuestionIndex >= questions.length - 1) {
-    removeLink('[data-next-link]');
-  } else {
-    setLinkHref(
-      '[data-next-link]',
-      questions[currentQuestionIndex + 1],
-      'Next Question'
-    );
-  }
+	if (currentQuestionIndex >= questions.length - 1) {
+		removeLink('[data-next-link]');
+	} else {
+		setLinkHref('[data-next-link]', questions[currentQuestionIndex + 1], 'Next Question');
+	}
 };
 
 const updateQuestionNumber = (currentQuestionIndex) => {
-  const currentQuestionElement = document.querySelector(
-    '[data-question-number]'
-  );
+	const currentQuestionElement = document.querySelector('[data-question-number]');
 
-  if (currentQuestionElement) {
-    currentQuestionElement.innerHTML = currentQuestionIndex + 1;
-  }
+	if (currentQuestionElement) {
+		currentQuestionElement.innerHTML = currentQuestionIndex + 1;
+	}
 };
 
-const updatePageTitle = (currentQuestionIndex) => {
-  document.title = document.title.replace(/\d+/, currentQuestionIndex + 1);
+const updateQuestionTotal = (total) => {
+	const questionTotalElement = document.querySelector('[data-question-total]');
+
+	if (questionTotalElement) {
+		questionTotalElement.innerHTML = total;
+	}
+};
+
+const updatePageTitle = (currentQuestionIndex, total) => {
+	let newTitle = document.title.replace(/\d+/, currentQuestionIndex + 1);
+	newTitle = newTitle.replace(/of\s\d+/, `of ${total}`);
+
+	document.title = newTitle;
 };
 
 export const updatePaginationLinks = () => {
-  const questionsRaw = sessionStorage.getItem('questions');
-  const currentQuestionIndexRaw = sessionStorage.getItem(
-    'currentQuestionIndex'
-  );
+	const questionsRaw = sessionStorage.getItem('questions');
+	const currentQuestionIndexRaw = sessionStorage.getItem('currentQuestionIndex');
 
-  if (questionsRaw && currentQuestionIndexRaw) {
-    const questions = JSON.parse(questionsRaw);
-    const currentQuestionIndex = Number.parseInt(currentQuestionIndexRaw, 10);
+	if (questionsRaw && currentQuestionIndexRaw) {
+		const questions = JSON.parse(questionsRaw);
+		const currentQuestionIndex = Number.parseInt(currentQuestionIndexRaw, 10);
 
-    updatePreviousLink(questions, currentQuestionIndex);
-    updateNextLink(questions, currentQuestionIndex);
-    updateQuestionNumber(currentQuestionIndex);
-    updatePageTitle(currentQuestionIndex);
+		updatePreviousLink(questions, currentQuestionIndex);
+		updateNextLink(questions, currentQuestionIndex);
+		updateQuestionNumber(currentQuestionIndex);
+		updateQuestionTotal(questions.length);
+		updatePageTitle(currentQuestionIndex, questions.length);
 
-    const previousLink = document.querySelector('[data-prev-link]');
-    previousLink?.addEventListener('click', (event) => {
-      event.preventDefault();
+		const previousLink = document.querySelector('[data-prev-link]');
+		previousLink?.addEventListener('click', (event) => {
+			event.preventDefault();
 
-      sessionStorage.setItem(
-        'currentQuestionIndex',
-        (currentQuestionIndex - 1).toString()
-      );
-      window.location.href = event.target.href;
-    });
+			sessionStorage.setItem('currentQuestionIndex', (currentQuestionIndex - 1).toString());
+			window.location.href = event.target.href;
+		});
 
-    const nextLink = document.querySelector('[data-next-link]');
-    nextLink?.addEventListener('click', (event) => {
-      event.preventDefault();
+		const nextLink = document.querySelector('[data-next-link]');
+		nextLink?.addEventListener('click', (event) => {
+			event.preventDefault();
 
-      sessionStorage.setItem(
-        'currentQuestionIndex',
-        (currentQuestionIndex + 1).toString()
-      );
-      window.location.href = event.target.href;
-    });
-  }
+			sessionStorage.setItem('currentQuestionIndex', (currentQuestionIndex + 1).toString());
+			window.location.href = event.target.href;
+		});
+	}
 };
